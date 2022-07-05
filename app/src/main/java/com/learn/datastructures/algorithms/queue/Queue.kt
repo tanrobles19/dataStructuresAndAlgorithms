a3341de53bc2d566ef711f9653970d15c4fef7b5
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-07-04 18:19:00 -0600

    Queue Interface and ArrayList Implementation

---
+++ app/src/main/java/com/learn/datastructures/algorithms/queue/Queue.kt
@@ -0,0 +1,16 @@
+package com.learn.datastructures.algorithms.queue
+
+interface Queue<T: Any> {
+
+    fun enqueue(element: T): Boolean
+
+    fun dequeue(): T?
+
+    val count: Int
+        get
+
+    val isEmpty: Boolean
+        get() = count == 0
+
+    fun peek(): T?
+}
